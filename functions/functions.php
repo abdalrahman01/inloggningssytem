91a79621c5523adab6fc9a1cc0250b70b330aea9
Abd Alrahman Atieh <abdalrahman.atieh@gmail.com>
2021-03-27 15:31:27 +0100

    added docs

--- functions/functions.php
+++ functions/functions.php
@@ -1,5 +1,13 @@
 <?php
 
+/**
+ * check_credintials
+ *
+ * @param  string $username
+ * @param  string $password
+ * @param  mixed $db
+ * @return void
+ */
 function check_credintials($username, $password, $db)
 {
 
@@ -31,6 +39,13 @@ function check_credintials($username, $password, $db)
     }
 }
 
+/**
+ * delete_post_by_id
+ *
+ * @param  mixed $id
+ * @param  mixed $db
+ * @return void
+ */
 function delete_post_by_id($id, $db){
     $sql = "DELETE FROM posts WHERE posts.post_id = :post_id";
     $stmt = $db -> prepare($sql);
@@ -38,6 +53,12 @@ function delete_post_by_id($id, $db){
     $stmt -> execute();
 }
 
+/**
+ * select_all_posts
+ *
+ * @param  mixed $db
+ * @return object
+ */
 function select_all_posts($db){
     $sql = "SELECT posts.post_id, posts.post_title, posts.post_content, posts.date, users.username 
     FROM posts INNER JOIN users ON users.user_id = posts.post_creator";
@@ -46,6 +67,13 @@ function select_all_posts($db){
     return $stmt;
 }
 
+/**
+ * select_post_by_id
+ *
+ * @param  mixed $id
+ * @param  mixed $db
+ * @return void
+ */
 function select_post_by_id($id, $db){
    
     $sql = 'SELECT * FROM posts WHERE post_id = :id';
@@ -56,6 +84,15 @@ function select_post_by_id($id, $db){
     return $row;        
 } 
 
+/**
+ * update_post
+ *
+ * @param  mixed $post_title
+ * @param  mixed $post_content
+ * @param  mixed $post_id
+ * @param  mixed $db
+ * @return void
+ */
 function update_post($post_title, $post_content, $post_id, $db){
     
     $sql = "UPDATE posts SET posts.post_title = :post_title, posts.post_content = :post_content WHERE posts.post_id = :post_id";
@@ -68,12 +105,21 @@ function update_post($post_title, $post_content, $post_id, $db){
 
 }
 
+/**
+ * insert_post
+ *
+ * @param  mixed $post_title
+ * @param  mixed $post_content
+ * @param  mixed $post_creator
+ * @param  mixed $db
+ * @return void
+ */
 function insert_post($post_title, $post_content, $post_creator, $db) {
     $sql = "INSERT INTO posts (post_creator, post_title, post_content, posts.date) VALUES (:post_creator, :post_title, :post_content, NOW())";
     $stmt = $db -> prepare($sql);
     $stmt->bindValue(":post_title", $post_title);
     $stmt->bindValue(":post_content", $post_content);
-    $stmt->bindValue(":post_creator", $post_creator, PDO::PARAM_INT);
+    $stmt->bindValue(":post_creator", $post_creator);
     $stmt->execute();
    
 }
