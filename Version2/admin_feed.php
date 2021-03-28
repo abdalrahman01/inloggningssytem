af90911c4566b0e692a28422159c223c2aa694b2
Abd Alrahman Atieh <abdalrahman.atieh@gmail.com>
2021-03-28 19:30:32 +0200

    style

--- Version2/admin_feed.php
+++ Version2/admin_feed.php
@@ -7,7 +7,7 @@ if ($_SESSION["permission_level"] != "admin") {
 }
 
 require_once "config/db.php";
-require_once "functions/post_functions.php"; 
+require_once "functions/post_functions.php";
 
 ?>
 
@@ -20,37 +20,60 @@ require_once "functions/post_functions.php";
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Admin</title>
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/grid.css">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+
 </head>
 
 <body>
 
-    <a href="logout.php">Logout!</a>
-    <a href="add.php">ADD A POST</a>
-    <a href="add_user.php">ADD A USer</a>
-    <a href="users_list.php">Users list</a>
-
-
-    <hr>
-
-    <?php
-        $stmt = select_all_posts($db);
-        if($stmt->rowCount() > 0){
-            while($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
-                $post_id = $row["post_id"];
-                echo '<h2>' . $row["post_title"] . '</h2>' . 
-                '<p>' . $row["post_content"] .  '</p>' . 
-                '<h4>' . $row["date"] . ' | ' . $row["username"] . '</h4>' . 
-                '<form action="edit.php" method="post">' . 
-                '<input type="hidden" name="post_id" value="'. $post_id .'">' . 
-                '<input type="submit" value="edit" name="id">' . 
-                ' </form>' ;
-                echo '<form action="delete.php" method="post">
-                <input type="hidden" name="post_id" value="'. $post_id .'">
+    <div class="container">
+        <div class="row">
+            <div class="posts col-9">
+                <?php
+                $stmt = select_all_posts($db);
+                if ($stmt->rowCount() > 0) {
+                    while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+                        $post_id = $row["post_id"];
+                        echo '<div class="post"> <h2>' . $row["post_title"] . '</h2>' .
+                            '<p>' . $row["post_content"] .  '</p>' .
+                            '<h4>' . $row["date"] . ' | ' . $row["username"] . '</h4>' .
+                            '<form action="edit.php" method="post">' .
+                            '<input type="hidden" name="post_id" value="' . $post_id . '">' .
+                            '<input type="submit" value="edit" name="id">' .
+                            ' </form>';
+                        echo '<form action="delete.php" method="post">
+                <input type="hidden" name="post_id" value="' . $post_id . '">
                 <input type="submit" name="id" value="Delete">
-            </form>';
-            }
-        }
-    ?>
+            </form> </div>';
+                    }
+                }
+                ?>
+            </div>
+
+            <div class="option-admin-feed col-2">
+                <div class="option-card">
+                    <a href="logout.php">Logout!</a>
+                </div>
+                <div class="option-card">
+                    <a href="add.php">ADD A POST</a>
+                </div>
+                <div class="option-card">
+                    <a href="add_user.php">ADD A USer</a>
+                </div>
+                <div class="option-card">
+                    <a href="users_list.php">Users list</a>
+                </div>
+            </div>
+
+        </div>
+    </div>
+    
+
+
+
+
 </body>
 
 </html>
