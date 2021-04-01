344df39ecf17f60adb4dcde7590f9658222089f0
Abd Alrahman Atieh <abdalrahman.atieh@gmail.com>
2021-04-01 19:35:40 +0200

    better style

--- Version2/index.php
+++ Version2/index.php
@@ -2,32 +2,32 @@
 
 
 session_start();
-if(isset($_SESSION["username"])){
+if (isset($_SESSION["username"])) {
     redirect_to_home($_SESSION["permission_level"]);
-
 }
 
 require_once "functions/user_functions.php";
 require_once "config/db.php";
 
-if(isset($_POST["login"])){
+if (isset($_POST["login"])) {
     check_credintials($_POST["username"], $_POST["password"], $db);
     redirect_to_home($_SESSION["permission_level"]);
 }
 
 
-function redirect_to_home($permission_level) {
-    if($permission_level == "admin") {
+function redirect_to_home($permission_level)
+{
+    if ($permission_level == "admin") {
         header("Location: admin_feed.php");
-
     }
-    if($permission_level == "user") {
+    if ($permission_level == "user") {
         header("Location: feed.php");
     }
 }
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -36,15 +36,24 @@ function redirect_to_home($permission_level) {
     <link rel="stylesheet" href="css/bootstrap.min.css">
     <link rel="stylesheet" href="css/style.css">
 </head>
+
 <body>
-    <div class="container a-grid">
-        <form action="." method="post" class="login-form">
-            <input type="text" name="username" placeholder="Username">
+    <div class="container center">
+        <form method="post" action=".">
+            <div class="form-group">
+                <label>Username</label>
+                <input type="text" class="form-control" name="username" placeholder="Enter username" required>
+            </div>
+            <div class="form-group">
+                <label>Password</label>
+                <input type="password" class="form-control" name="password" placeholder="Password" required>
+            </div>
             <br>
-            <input type="password" name="password" placeholder="password">
-            <br>
-            <input type="submit" value="Log in" name="login">
+            <input type="submit" value="Log in" name="login" class="btn btn-primary">
+            <span>Or register</span> <a href="register.php">Register me</a>
+
         </form>
     </div>
 </body>
+
 </html>
