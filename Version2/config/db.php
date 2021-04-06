38234c91003aa2bc53b427d0c8e9fe3df77b23f3
Abd Alrahman Atieh <abdalrahman.atieh@gmail.com>
2021-04-06 18:02:26 +0200

    added a try stmt if db doesnt excist

--- Version2/config/db.php
+++ Version2/config/db.php
@@ -1,13 +1,9 @@
 <?php
- $db = new PDO(
-    'mysql:host=127.0.0.1;dbname=inloggning;charset=utf8',
-    'root',
-    ''
-);
-
-// $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_SILENT);
-// $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
+try {
+    $db = new PDO('mysql:host=127.0.0.1;dbname=inloggning;charset=utf8','root','');
+} catch (Exception $e) {
+    header("Location: help.html#connection_denied");
+}   
 $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-// den gör att PDO tolkar inte queries innan dem skickas 
 $db->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+
